415136f0626d58fb5e08fc5e6419c378f8a36576
liangyali <me@liangyali.com>
2017-09-14 16:07:49 +0800

    fix

--- install/ec2.sh
+++ install/ec2.sh
@@ -9,6 +9,7 @@ sudo curl -L https://get.daocloud.io/docker/compose/releases/download/1.11.2/doc
 sudo chmod +x /usr/local/bin/docker-compose
 
 # 配置docker文件
+sudo mkdir /etc/docker
 sudo echo '{"registry-mirrors":["http://3fcf853c.m.daocloud.io"],"insecure-registries":["54.223.52.77:5000"]}' | sudo tee /etc/docker/daemon.json
 
 sleep 3
